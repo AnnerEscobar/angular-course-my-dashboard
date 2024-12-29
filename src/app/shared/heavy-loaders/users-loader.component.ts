c7e4970122c95dc6f698e423e2feb26bf5a42222
Anner Escobar <anner123escobar@gmail.com>
2024-12-28 22:08:32 -0600

    directorios creados

---
+++ src/app/shared/heavy-loaders/users-loader.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app*user-loader',
+  imports: [],
+  template: `<h1>users-loader</h1>`,
+})
+
+export class UsersLoaderComponent {
+
+  constructor() { }
+
+}
