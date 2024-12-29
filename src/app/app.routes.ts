b4d38ebfe2c2e00cbfb4f9c78ae7d9dd61ba5d6c
Anner Escobar <anner123escobar@gmail.com>
2024-12-28 22:30:00 -0600

    creacion del router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,56 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./dahsboard/dashboard.component'),
+    children: [
+      {
+        path: 'change-detection',
+        title: 'Change Detection',
+        loadComponent: () => import('./dahsboard/pages/change-detection/change-detection.component'),
+      },
+      {
+        path: 'control-flow',
+        title: 'Control Flow',
+        loadComponent: () => import('./dahsboard/pages/control-flow/control-flow.component'),
+      },
+      {
+        path: 'defer-options',
+        title: 'Defer Options',
+        loadComponent: () => import('./dahsboard/pages/defer-options/defer-options.component'),
+      },
+      {
+        path: 'defer-view',
+        title: 'Defer View',
+        loadComponent: () => import('./dahsboard/pages/defer-views/defer-views.component'),
+      },
+      {
+        path: 'user/:id',
+        title: 'User',
+        loadComponent: () => import('./dahsboard/pages/user/user.component'),
+      },
+      {
+        path: 'users-list',
+        title: 'Users List',
+        loadComponent: () => import('./dahsboard/pages/users/users.component'),
+      },
+      {
+        path: 'view-transition',
+        title: 'View Transition',
+        loadComponent: () => import('./dahsboard/pages/view-transition/view-transition.component'),
+      },
+      {
+        path: '', redirectTo: 'control-flow', pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  }
+
+];
